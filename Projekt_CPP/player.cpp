da690d9b0682d595a52df1e1fa9e0531df8f7365
Evgeniia <zhenya.volkova.01@icloud.com>
2021-12-07 17:41:10 +0100

    add read() and write() -> class player; json player

--- Projekt_CPP/player.cpp
+++ Projekt_CPP/player.cpp
@@ -4,3 +4,15 @@ Player::Player()
 {
 
 }
+
+void Player::read(const QJsonObject &json){
+    if (json.contains("name") && json["name"].isString())
+        m_name = json["name"].toString();
+    if (json.contains("money") && json["money"].isDouble())
+        m_money = json["money"].toInt();
+};
+
+void Player::write(QJsonObject &json) const{
+    json["name"] = m_name;
+    json["money"] = m_money;
+};
