7b535bf6de45745769ff54e541b196a6fc24e7c5
Evgeniia <zhenya.volkova.01@icloud.com>
2021-12-07 15:37:26 +0100

    save and load
    player, inventory, seed

--- Projekt_CPP/player.h
+++ Projekt_CPP/player.h
@@ -2,15 +2,21 @@
 #define PLAYER_H
 #include <iostream>
 #include "inventory.h"
+#include <QString>
 
 class Player
 {
 private:
-    std::string m_name;
+    QString m_name;
     Inventory* m_bag;
     int m_money;
+    //int level;
+
 public:
     Player();
+    Player(QString name,int money);
+
+
 };
 
 #endif // PLAYER_H
